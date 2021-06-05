423afdde46242742b0907ac9d428825a62a662d5
João Cencio <joaovictor.cencio@gmail.com>
2021-06-04 23:25:12 -0300

    construtor e função da classe

--- src/main/kotlin/one/digitalinnovation/digiobank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digiobank/Pessoa.kt
@@ -4,10 +4,13 @@ class Pessoa {
     var nome:String = "Jether"
     var cpf:String = "123.123.123-12"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main(){
     val jether = Pessoa()
-    println(jether.nome)
-    println(jether.cpf)
+    println(jether.pessoaInfo())
 }
